7e197f130a00d271b35cc2ffb86f82e9dd0d0c34
lucsm3 <lucsm3@gmail.com>
2024-06-13 21:54:41 -0300

    "exemplo de custom serialize, como mudar o nome somente no retorno do json e ocultar campos que nao quer que seja enviado"

--- src/main/java/br/com/lucas/data/vo/v1/PersonVO.java
+++ src/main/java/br/com/lucas/data/vo/v1/PersonVO.java
@@ -1,14 +1,20 @@
 package br.com.lucas.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serializable;
 import java.util.Objects;
-
+@JsonPropertyOrder({"id,adress,first_name,lastName,address,gender"})
 public class PersonVO implements Serializable {
     private static final long serialVersionUID = 1L;
     private Long id;
+    @JsonProperty("first_name")
     private String firstName;
     private String lastName;
     private String address;
+    @JsonIgnore
     private String gender;
 
     public Long getId() {
